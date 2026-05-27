f8d9ebabc964291d6d08ef72b69620060cfe8859
Maciej Wiśniewski <maciej.wisniewski@aol.com>
2026-05-27 10:49:23 +0200

    fix: remove unnecessary Vue component from Vite script in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -45,7 +45,7 @@
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
 
-    @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
+    @vite(['resources/js/app.ts'])
     @inertiaHead
 </head>
 
